a59e9bb16b284122039c9290b471d36e6e68c1e3
Conner Huff <connerhuff@Conners-MacBook-Pro-2.local>
2016-02-24 12:29:42 -0600

    Finished lab

---
+++ app/src/main/java/com/cs407_android/ormlab/ExampleDaoGenerator.java
@@ -0,0 +1,33 @@
+package com.cs407_android.ormlab;
+
+/**
+ * Created by njaunich on 2/18/16.
+ */
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+
+public class ExampleDaoGenerator {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.cs407_android.ormlab"); //Scheme for GreenDAO ORM
+        createDB(schema);
+        new DaoGenerator().generateAll(schema, "./app/src/main/java/");
+    }
+
+    private static void createDB(Schema schema) {
+
+        //Add Guest
+        Entity guest = schema.addEntity("Guest");
+        guest.addIdProperty();
+        //TODO: Add the following string properties to the Guest entity
+        //TODO: (cont.) firstName, lastName, email, phone
+        guest.addStringProperty("firstName");
+        guest.addStringProperty("lastName");
+        guest.addStringProperty("email");
+        guest.addStringProperty("phone");
+        //TODO: Add a Boolean property named "display" (this will always be set to true)
+        guest.addBooleanProperty("display");
+    }
+
+}
